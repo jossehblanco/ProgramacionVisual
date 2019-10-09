3c912f3f6d8ce2393046a9bbc77c16af43aea8e1
jossehblanco <jossehblanco@gmail.com>
2019-10-09 00:50:14 -0600

    añadiendo dependencias de filesaver.js y matdialog

--- teoangular/src/app/app.module.ts
+++ teoangular/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 
 import { DiagramaComponent } from './diagrama/diagrama.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatAutocompleteModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatInputModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatTooltipModule} from '@angular/material'
-import {MatToolbarModule} from '@angular/material'
+import {MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatAutocompleteModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatInputModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatTooltipModule, MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material'
+import {MatToolbarModule} from '@angular/material';
+import { MatDialogCComponent } from './mat-dialog-c/mat-dialog-c.component'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiagramaComponent,
+    MatDialogCComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     MatSidenavModule,
     MatToolbarModule,
@@ -51,7 +55,13 @@ import {MatToolbarModule} from '@angular/material'
     MatTooltipModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  entryComponents: [
+    MatDialogCComponent
+  ], 
+  providers: [
+    {provide : MAT_DIALOG_DEFAULT_OPTIONS, useValue:
+    {hasBackdrop : true}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
